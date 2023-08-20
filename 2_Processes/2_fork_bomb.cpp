cd29301ff53adf67f1f603dc37b47fe041c758fb
Alexander <oudetore@mail.ru>
2023-08-20 20:13:02 +0300

    hw 3

---
+++ 2_Processes/2_fork_bomb.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    fork();
+    fork();
+    fork();
+    printf("hello\n");
+    return 0;
+}
+
+/*
+        L1
+       /  \
+      L2  L2
+     / \  / \
+    L3 L3L3 L3
+ */
